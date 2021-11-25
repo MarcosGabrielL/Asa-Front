ed4078db6653b4348aa03c55410ccb1474570f06
MarcosGabrielL <marcos_gabrielsaj@hotmail.com>
2021-11-25 19:35:34 -0300

    Att Views/Css

--- src/app/components/views/movie/movie-page/movie-page.component.ts
+++ src/app/components/views/movie/movie-page/movie-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { OwlOptions } from 'ngx-owl-carousel-o';
 
 @Component({
   selector: 'app-movie-page',
@@ -10,6 +11,22 @@ export class MoviePageComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+   customOptions: OwlOptions = {
+    loop: true,
+    mouseDrag: false,
+    touchDrag: false,
+    pullDrag: false,
+    dots: false,
+    navSpeed: 600,
+    navText: ["<i style='font-size: 28px;' class='fa fa-chevron-left'></i>", "<i style='font-size: 28px;' class='fa fa-chevron-right'></i>"],
+    responsive: {
+      0: {
+        items: 1 
+      }
+    },
+    nav: true
   }
 
 }
