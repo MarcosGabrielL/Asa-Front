f8501b414c24afeafca0874e797cd554af730f48
MarcosGabrielL <marcos_gabrielsaj@hotmail.com>
2021-12-06 11:06:41 -0300

    06-12 11:06

--- server.js
+++ server.js
@@ -6,10 +6,10 @@ const path = require('path');
 const app = express();   
 
 // Serve only the static files form the dist directory    
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/asafrontapp'));
 
 app.get('/*', function(req,res) {  
-    res.sendFile(path.join(__dirname+'/dist/index.html'));   
+    res.sendFile(path.join(__dirname+'/dist/asafrontapp/index.html'));   
 });  
 
 // Start the app by listening on the default Heroku port    
