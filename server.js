00b80caa1cafa0894a8f8a204f3c82a7a3eae592
MarcosGabrielL <marcos_gabrielsaj@hotmail.com>
2021-12-06 09:50:18 -0300

    06-12 09:50

--- server.js
+++ server.js
@@ -1,22 +1,19 @@
-function requireHTTPS(req, res, next) {
-    // The 'x-forwarded-proto' check is for Heroku
-    if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
-        return res.redirect('https://' + req.get('host') + req.url);
-    }
-    next();
-}
+//Install express server    
 const express = require('express');
-const app = express();
-const app = express();
-app.use(requireHTTPS);
 
-app.use(express.static(’./dist/asafrontapp’));
+const path = require('path');   
 
-app.get('/*', function(req, res) {
-  res.sendFile(’index.html’, {root: 'dist/asafrontapp/’}
-);
-});
+const app = express();   
 
+// Serve only the static files form the dist directory    
+app.use(express.static(__dirname + '/dist'));
+
+app.get('/*', function(req,res) {  
+    res.sendFile(path.join(__dirname+'/dist/index.html'));   
+});  
+
+// Start the app by listening on the default Heroku port    
+app.listen(process.env.PORT || 8080);
 
 
 
