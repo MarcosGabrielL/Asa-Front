cb13fca755a97bf47d12aa337c558c37c71db2d0
MarcosGabrielL <marcos_gabrielsaj@hotmail.com>
2021-12-06 09:28:23 -0300

    d

--- server.js
+++ server.js
@@ -1,8 +1,23 @@
+function requireHTTPS(req, res, next) {
+    // The 'x-forwarded-proto' check is for Heroku
+    if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
+        return res.redirect('https://' + req.get('host') + req.url);
+    }
+    next();
+}
 const express = require('express');
-const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/asafrontapp'));
-app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname+
-'/dist/asafrontapp/index.html'));});
-app.listen(process.env.PORT || 5000);
+const app = express();
+app.use(requireHTTPS);
+
+app.use(express.static(’./dist/asafrontapp’));
+
+app.get('/*', function(req, res) {
+  res.sendFile(’index.html’, {root: 'dist/asafrontapp/’}
+);
+});
+
+
+
+
+
