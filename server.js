96583961a5922e0e1fc607a41ac0e17602239826
MarcosGabrielL <marcos_gabrielsaj@hotmail.com>
2021-12-06 09:07:01 -0300

    06-12 09:06

--- server.js
+++ server.js
@@ -5,4 +5,3 @@ app.use(express.static(__dirname + '/dist/asafrontapp'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
 '/dist/asafrontapp/index.html'));});
-app.listen(process.env.PORT || 8080);
