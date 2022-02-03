14260f03627d11abf15f098f6db8f7bfe341aff0
Marcos Gabriel <52946597+MarcosGabrielL@users.noreply.github.com>
2022-02-03 05:24:10 -0300

    Update home-geral-filmes.service.spec.ts

--- src/app/components/views/home/home-geral-filmes/home-geral-filmes.service.spec.ts
+++ src/app/components/views/home/home-geral-filmes/home-geral-filmes.service.spec.ts
@@ -14,3 +14,20 @@ describe('HomeGeralFilmesService', () => {
     expect(service).toBeTruthy();
   });
 });
+
+
+@Injectable()
+export class MyService {
+    myMethod$: Observable<any>;
+    private myMethodSubject = new Subject<any>();
+
+    constructor() {
+        this.myMethod$ = this.myMethodSubject.asObservable();
+    }
+
+    myMethod(data) {
+        console.log(data); // I have data! Let's return it so subscribers can use it!
+        // we can do stuff with data if we want
+        this.myMethodSubject.next(data);
+    }
+}
