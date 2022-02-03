efb57e4153fb08ea6f2e8ec3225e8d51ca52fd97
Marcos Gabriel <52946597+MarcosGabrielL@users.noreply.github.com>
2022-02-03 07:49:36 -0300

    Update home.component.ts

--- src/app/components/views/home/home.component.ts
+++ src/app/components/views/home/home.component.ts
@@ -11,38 +11,14 @@ export class HomeComponent implements OnInit {
 
   constructor( private router: Router, private route: ActivatedRoute, private service: HomeGeralFilmesService) { }
 
-    m1 = false;
-  m2 = false;
-  m3 = false;
-  m4 = false;
-  m5 = false;
-  m6 = false;
-  m7 = false;
-  m8 = false;
-  m9 = false;
-  m10 = false;
-  m11 = false;
-  m12 = false;
-  m13 = false;
-  m14 = false;
-  m15 = false;
-  m16 = false;
-  m17 = false;
-  m18 = false;
-  m19 = false;
+   
    genero: string = "";
   
   ngOnInit(): void {
                 this.genero =  this.service.data;
     
-     if(this.genero === null){
-      this.m1 = true;
-    }    
-    if(this.genero === "28"){
-      this.m2 = true;
-    }
     
-    //console.log(this.genero);
+    console.log(this.genero);
     //console.log("O");
   }
 
