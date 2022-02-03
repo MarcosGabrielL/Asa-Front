85da1fe7f196a736af2a6a508037bc419fe4365d
Marcos Gabriel <52946597+MarcosGabrielL@users.noreply.github.com>
2022-02-03 04:54:51 -0300

    Update home.component.ts

--- src/app/components/views/home/home.component.ts
+++ src/app/components/views/home/home.component.ts
@@ -14,7 +14,7 @@ export class HomeComponent implements OnInit {
  m1 = true;
   
   ngOnInit(): void {
-    console.log(this.route.snapshot.paramMap.get("genero"));
+    console.log(this.router.snapshot.paramMap.get("genero"));
     console.log("O");
   }
 
